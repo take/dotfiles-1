4f0234bcc11aa7bed82cdd00fd93dd5e35790251
Takehiro Adachi <takehiro0740@gmail.com>
2015-05-03 20:12:14 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -26,14 +26,13 @@ which brew > /dev/null
 if [ "$?" -ne 0 ]; then
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
-sudo chown root /usr/local/bin/brew
 
 # Install serverkit and its dependencies
-sudo which bundle > /dev/null || sudo gem install bundler
-sudo bundle install > /dev/null
+which bundle > /dev/null || gem install bundler
+bundle install > /dev/null
 
 # Run installer
-sudo bundle exec serverkit apply recipe.yml.erb --variables=variables.yml
+bundle exec serverkit apply recipe.yml.erb --variables=variables.yml
 
 # Move to original path
 popd > /dev/null
